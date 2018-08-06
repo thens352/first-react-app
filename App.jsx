e3af3250809f4fb3f9b0202aab1c3b8a761225ab
Enes KURU <enes793k@gmail.com>
2018-08-06 07:27:26 +0300

    example 3 commit

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-class App extends React.Component {
+//example 1
+/*class App extends React.Component {
   render() {
     let i = 1;
     const myStyle = {
@@ -9,8 +10,6 @@ class App extends React.Component {
     };
     return (
       <div>
-        {//End of the line Comment...}
-          {/*Multi line comment...*/}
         <h1 style={{color:'#FF00FF'}}>Header</h1>
         <h1 style = {myStyle}>Styled Header</h1>
         <h1>i === 1 => {i === 1 ? 'True!' : 'False'}</h1>
@@ -19,5 +18,121 @@ class App extends React.Component {
       </div>
     );
   }
+}*/
+//end example 1---------------------------------------------------------------------
+
+//example 2
+/*class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+        <Content/>
+      </div>
+    );
+  }
 }
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Header</h1>
+      </div>
+    );
+  }
+}
+class Content extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Content</h2>
+        <p>The content text!!!</p>
+      </div>
+    );
+  }
+}*/
+//end example 2-----------------------------------
+
+//example 3
+/*
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Header</h1>
+      </div>
+    );
+  }
+}
+
+class TableRow extends React.Component {
+  render() {
+    return (
+      <tr>
+        <td>{this.props.data.id}</td>
+        <td>{this.props.data.name}</td>
+        <td>{this.props.data.age}</td>
+      </tr>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data:
+        [
+          {
+            "id":1,
+            "name":"Foo",
+            "age":"20"
+          },
+          {
+            "id":2,
+            "name":"Bar",
+            "age":"30"
+          },
+          {
+            "id":3,
+            "name":"Baz",
+            "age":"40"
+          }
+        ]
+    }
+  }
+  render() {
+    return (
+      <div>
+        <Header/>
+        <table>
+          <tbody>
+          {this.state.data.map((person, i) => <TableRow key = {i} data = {person} />)}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}*/
+//end example 3-----------------------------------
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      header: "Header from state...",
+      content: "Content from state..."
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.header}</h1>
+        <h2>{this.state.content}</h2>
+      </div>
+    );
+  }
+}
+
 export default App;
