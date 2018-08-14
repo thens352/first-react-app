b3f75cf7fce071ddfad4ca589614cf935cd05189
thens352 <enes793k@gmail.com>
2018-08-14 17:53:20 +0300

    form example added

--- App12.jsx
+++ App12.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 //example 12
-function ListItem(props) {
+/*function ListItem(props) {
   return <li>{props.value}</li>;
 }
 
@@ -25,6 +25,110 @@ class App extends React.Component {
       <NumberList numbers={this.state.numbers}/>
     );
   }
+}*/
+
+//example 12-2
+/*class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    //this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  /!*handleChange(event) {
+    this.setState({value: event.target.value});
+  }*!/
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          {/!*<input type="text" value={this.state.value} onChange={this.handleChange} />*!/}
+          <input type="text" value={this.state.value} onChange={(event) => this.setState({value: event.target.value})}/>
+        </label>
+        <input type="submit" value="Submit"/>
+      </form>
+    );
+  }
+}*/
+
+//example 12-3
+/*class FlavorForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'coconut'};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Your favorite flavor is: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Pick your favorite flavor:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}*/
+
+//example 12-4
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p>The water would boil.</p>;
+  }
+  return <p>The water would not boil.</p>;
 }
 
-export default App;
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {temperature: ''};
+  }
+
+  handleChange(e) {
+    this.setState({temperature: e.target.value});
+  }
+
+  render() {
+    const temperature = this.state.temperature;
+    return (
+      <fieldset>
+        <legend>Enter temperature in Celsius:</legend>
+        <input
+          value={temperature}
+          onChange={this.handleChange} />
+        <BoilingVerdict
+          celsius={parseFloat(temperature)} />
+      </fieldset>
+    );
+  }
+}
+
+export default Calculator;
